21549dd7bc80e858cf13a8e08aa71b21e1575784
haikalfadle1827-rgb <haikalfadle1827@gmail.com>
2025-09-13 11:54:52 +0700

    250441100005_Achmad Haikal Fadlillah_day 1

---
+++ 250441100005_Achmad Haikal Fadlillah_day 1/pelcood.py
@@ -0,0 +1,38 @@
+# nama = "Achmad Haikal Fadlillah"
+# nim = 250441100005
+# semester = 1
+# SD = "SDN Balongmojo"
+# SMP = "SMPN 28 Gresik"
+# SMA = "SMAN 1 Kedamean"
+
+# print("====BIODATA MAHASISWA====")
+# print("Hallo, nama saya : "+ nama + "\n")
+# print(f"NIM saya : {nim}\n")
+# print("Saya berkuliah di semester : {}".format(semester))
+
+# print("====RIWAYAT PENDIDIKAN====")
+# print("SD : "+ SD + "\n")
+# print("SMP : "+ SMP + "\n")
+# print("SMA : "+ SMA + "\n")
+
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b
+# e = a * b 
+# f = a / b 
+# g = a % b
+
+# print("Hasil Penjumlahan:", c)
+# print("Hasil Pengurangan:", d)
+# print("Hasil Perkalian:", e)
+# print("Hasil Pembagian:", f)
+# print("Hasil Sisa Bagi:", g)
+
+nama = input("masukkan nama anda : ")
+umur = input("masukkan umur anda : ")
+buah = input("masukkan buah favorit : ")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur + " tahun")
+print("buah favorit saya adalah " + buah)
