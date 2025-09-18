c48105aea56a9ce3d0645e56a6d2c8f921607404
haikalfadle1827-rgb <haikalfadle1827@gmail.com>
2025-09-18 10:59:22 +0700

    mengumpulkan tugas pelcod day 1

---
+++ 250441100005_Achmad Haikal Fadlillah_day 1/Tugas pelcod.py
@@ -0,0 +1,19 @@
+print ("===Nilai yang Andi peroleh pada matkul Algoritma Pemrograman===")
+bobot_tugas = 30/100
+bobot_UTS = 30/100
+bobot_UAS = 40/100
+
+tugas = 85
+UTS = 75
+UAS = 90
+
+nilai_akhir_tugas = tugas * bobot_tugas
+nilai_akhir_UTS = UTS * bobot_UTS
+nilai_akhir_UAS = UAS * bobot_UAS
+nilai_akhir_keseluruhan = nilai_akhir_tugas + nilai_akhir_UTS + nilai_akhir_UAS
+
+print("nilai akhir tugas:", nilai_akhir_tugas)
+print("nilai akhir UTS :", nilai_akhir_UTS)
+print("nilai akhir UAS", nilai_akhir_UAS)
+print("nilai keseluruhan", nilai_akhir_keseluruhan)
+print("jadi nilai akhir yang Andi dapatkan pada matkul Algoritma Pemrograman adalah", nilai_akhir_keseluruhan)
